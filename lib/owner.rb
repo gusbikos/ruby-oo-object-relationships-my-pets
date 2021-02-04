bfadec95af6b3106fb246cf24ea5c000c2868020
gusbikos <gusbikos@gmail.com>
2021-02-04 17:19:08 -0500

    added changes (or any message you would like)

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
+require 'pry'
+require_relative 'cat'
+require_relative 'dog'
+
+
 class Owner
-  # code goes here
+  
+  @@all = []
+  
+
+  attr_reader :name, :species, :say_species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self 
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def self.count 
+    @@all.count 
+  end
+
+  def self.reset_all 
+    @@all = [] 
+  end
+
 end
+
+
